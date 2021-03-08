d516bb7f6ee13a00d99bb93f286debaa6669852b
Tobias Happ <tobias.happ@gmx.de>
2021-03-08 23:12:40 +0100

    Add basic app

---
+++ src/main/kotlin/de/tobiashapp/coinbase/plans/CoinbasePlansApplication.kt
@@ -0,0 +1,11 @@
+package de.tobiashapp.coinbase.plans
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CoinbasePlansApplication
+
+fun main(args: Array<String>) {
+	runApplication<CoinbasePlansApplication>(*args)
+}
---
+++ src/test/kotlin/de/tobiashapp/coinbase/plans/CoinbasePlansApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.tobiashapp.coinbase.plans
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CoinbasePlansApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
