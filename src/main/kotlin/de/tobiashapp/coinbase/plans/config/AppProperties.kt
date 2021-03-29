6b576dd1a2a459c61c93c459aa5ea12aca8389ce
Tobias Happ <tobias.happ@gmx.de>
2021-03-30 00:01:49 +0200

    Add plans properties

--- src/main/kotlin/de/tobiashapp/coinbase/plans/config/AppProperties.kt
+++ src/main/kotlin/de/tobiashapp/coinbase/plans/config/AppProperties.kt
@@ -1,13 +1,17 @@
 package de.tobiashapp.coinbase.plans.config
 
+import de.tobiashapp.coinbase.plans.models.CryptoCurrency
+import de.tobiashapp.coinbase.plans.models.FiatCurrency
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
+import java.math.BigDecimal
 
 @ConfigurationProperties(prefix = "app")
 @ConstructorBinding
 data class AppProperties(
     val coinbase: CoinbaseProperties,
     val mail: MailProperties,
+    val plans: List<PlanProperties>?,
 )
 
 data class CoinbaseProperties(
@@ -18,3 +22,11 @@ data class CoinbaseProperties(
 )
 
 data class MailProperties(val sender: String)
+
+data class PlanProperties(
+    val amount: BigDecimal,
+    val cron: String,
+    val cryptoCurrency: CryptoCurrency,
+    val fiatCurrency: FiatCurrency,
+    val receiverAddress: String,
+)
